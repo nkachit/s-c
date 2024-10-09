6c5ff83e387bbe4f006d3ce73328cdd62a5a2cb9
kriazfar <168362541+nkachit@users.noreply.github.com>
2024-10-09 12:07:18 -0300

    dsa

---
+++ perguntas.js
@@ -0,0 +1,80 @@
+criaCartao ('Matemática', 
+    '1. Quais a letra mais utilizada para indicar uma incógnita?',
+    'x')
+
+criaCartao ('Programação',
+    '2. O que é um loop em programação?',
+    'Um loop é uma estrutura que repete um bloco de código várias vezes até que uma condição seja atendida ou deixe de ser verdadeira.')
+
+criaCartao ('Química',
+    '3. O que é um átomo?',
+    'É a menor parte que forma tudo ao nosso redor.')
+
+criaCartao('Biologia',
+    '4. O que é fotossíntese?',
+    'É o processo pelo qual as plantas usam a luz do sol para fazer comida.')
+
+criaCartao ('História',
+     '5. Quanto tempo durou a Ditadura Civil-Militar?',
+     '21 anos.')
+
+criaCartao('Português'
+    ,'6. O que é um poema?',
+    'É uma forma de expressão em forma de palavras para que haja harmonia.')
+
+criaCartao('Geografia',
+    '7. Quantos continentes há?',
+    'Existem 6, sendo eles: América, África, Europa, Ásia, Oceania e Antártica')
+
+criaCartao('Matemática',
+    '8. Qual é a fórmula da área de um triângulo?',
+    'É a base vezes a altura, dividido por 2.')
+
+criaCartao('Programação',
+    '9. O que é uma variável em programação?',
+    'Uma variável é um nome que usamos para guardar um valor, como um número ou uma palavra, no código.')
+
+criaCartao('Química',
+    '10. O que é um elemento químico?',
+    'uma substância básica, como ouro ou oxigênio.')
+
+criaCartao ('Matemática',
+    ' 11. Matemática-Qual o valor aproximado de PI?',
+    '3.14')
+
+criaCartao('História',
+    '13. Qual foi o presidente que assumiu após a morte de Getúlio Vargas?',
+'Dutra')
+
+criaCartao('Biologia',
+    '14. Quem foi Mendel?',
+     'Biólogo e botânico austríaco')
+
+criaCartao('Química',
+    'O que é ligação covalente?',
+    '15. Uma ligação covalente é a união entre dois átomos que compartilham pares de elétrons para alcançar maior estabilidade.')
+
+criaCartao('Português',
+    '16. O que é uma metáfora?',
+    'Uma metáfora é uma comparação implícita entre duas coisas.')
+
+criaCartao('Programação',
+    '17.Qual é a saída do seguinte código em Python? ',
+    ' A saída é `[1, 3, 5]`. O fatiamento `x[::2]` seleciona todos os elementos da lista `x` com um passo de 2.')
+
+criaCartao('Química',
+    '18.O que é um ácido?',
+    'É uma substância que libera íons hidrogênio (H⁺) em solução aquosa.')
+
+criaCartao('História',
+    '19. O que foi a Inconfidência Mineira?',
+    'Foi uma tentativa de independência em Minas Gerais no final do século XVIII contra o domínio português.')
+
+criaCartao('Geografia',
+    '20. Quais países formam o BRICS?',
+    'Brasil, Rússia, Índia, China e África do Sul')
+
+
+criaCartao('Biologia',
+    'Qual é a função das mitocôndrias?',
+    'Produzir energia (ATP) através da respiração celular.')
