fa3bb0c7cc6aaa1a5d6a69763dae1fab16193c15
kriazfar <168362541+nkachit@users.noreply.github.com>
2024-10-29 08:27:07 -0300

    a

--- projeto2.js
+++ projeto2.js
@@ -17,7 +17,7 @@ function criaCartao(categoria, pergunta, resposta){
 }
 
 function mostraRessposta(card){
-    const resposta = card.querySelector('.resposta.cartao')
+    const resposta = card.querySelector('.resposta-cartao')
     if(resposta.style.display === 'none'){
       resposta.style.display = 'block';
     }
